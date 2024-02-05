4162cf57ae0d80e81aff8536cd0bf22e9d8ce8af
Chasqui10 <chasqui.rmf@gmail.com>
2024-02-04 20:38:18 -0700

    Adding previous work

--- server.js
+++ server.js
@@ -12,82 +12,112 @@ const db = mysql.createConnection(
   {
     host:'localhost',
     user: 'root',
-    password: 'PooPerS954!1Ch4RmF',
+    password: 'root',
     database: 'work_structure_db'
   },
   console.log(`Connected to the work_structure_db database.`)
 );
 
+function init() {
+
+  const questions = [
+    {
+      type: 'list',
+      name: 'index',
+      message: 'Where would you like to navigate to?',
+      choices: [
+        '(Department) View All',
+        '(Department) Add New',
+        '(Employee) View All',
+        '(Employee) Add New',
+        '(Employee) Update Role',
+        '(Role) View All',
+        '(Role) Add New'
+      ]
+    }
+    // {
+    //   type: 'input',
+    //   name: 'new_dept',
+    //   messsage: 'What is the name of the new department you would like to add?'
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'new_employee',
+    //   messsage: 'What is the name of the new employee you would like to add?'
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'new_role',
+    //   messsage: 'What is the name of the new role you would like to add?'
+    // },
+  ];
+
+
+  inquirer.prompt(questions).then((answers) => {
+    switch(answers.index) {
+      case '(Department) View All':
+        console.log('You chose (Department) View All');
+        db.query('SELECT * FROM department', function (err,results){
+          console.log(results);
+        });
+        break;
+      
+  
+      case '(Department) Add New':
+        console.log('You chose (Department) Add New');
+        break;
+  
+  
+      case '(Employee) View All':
+        console.log('You chose (Employee) View All');
+        db.query('SELECT * FROM employee', function (err,results){
+        console.log(results);
+        });
+        break;
+  
+  
+      case '(Employee) Add New':
+        console.log('You chose (Employee) Add New');
+        break;
+  
+  
+      case '(Employee) Update Role':
+        console.log('You chose (Employee) Update Role');
+        break;
+  
+  
+      case '(Role) View All':
+        console.log('You chose (Role) View All');
+        db.query('SELECT * FROM role', function (err,results){
+          console.log(results);
+        });
+        break;
+  
+  
+      case '(Role) Add New':
+        console.log('You chose (Role) Add New');
+        break;
+  
+  
+      default:
+        console.group('Invalid Choice')
+    }
+  }).catch((error) => {
+    console.log('Error Occured', error)
+  });
+
+
+
+};
+
+init();
+
+
+
+
+
 // inquirer prompt for terminal to gather data on the usage of the 
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'index',
-    message: 'Where would you like to navigate to?',
-    choices: 
-    [
-      '(Department) View All',
-      '(Department) Add New',
-      '(Employee) View All',
-      '(Employee) Add New',
-      '(Employee) Update Role',
-      '(Role) View All',
-      '(Role) Add New'
-    ],
-  }
-])
-.then((answers) => {
-  switch(answers.index) {
-    case '(Department) View All':
-      console.log('You chose (Department) View All');
-      db.query('SELECT * FROM department', function (err,results){
-        console.log(results);
-      });
-      break;
-    
 
-    case '(Department) Add New':
-      console.log('You chose (Department) Add New');
-      break;
-
-
-    case '(Employee) View All':
-      console.log('You chose (Employee) View All');
-      db.query('SELECT * FROM employee', function (err,results){
-      console.log(results);
-      });
-      break;
-
-
-    case '(Employee) Add New':
-      console.log('You chose (Employee) Add New');
-      break;
-
-
-    case '(Employee) Update Role':
-      console.log('You chose (Employee) Update Role');
-      break;
-
-
-    case '(Role) View All':
-      console.log('You chose (Role) View All');
-      db.query('SELECT * FROM role', function (err,results){
-        console.log(results);
-      });
-      break;
-
-
-    case '(Role) Add New':
-      console.log('You chose (Role) Add New');
-      break;
-
-
-    default:
-      console.group('Invalid Choice')
-  }
-}).catch((error) => {
-  console.log('Error Occured', error)
-});
 
 
 
